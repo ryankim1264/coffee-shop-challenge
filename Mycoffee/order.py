38ede431fea605ad73d35528ad483d109510b724
Ryan mugei <Lquid1264@gmail.com>
2025-05-21 00:38:13 +0300

    coffee-shop

--- Mycoffee/order.py
+++ Mycoffee/order.py
@@ -9,19 +9,19 @@ class Order:
         if isinstance (customer, str):
             self.customer = client.Customer(customer)
         else :
-            raise ValueError("must be a customer") 
+            raise TypeError("must be a customer") 
      
      
         if isinstance (coffee , str):   
              self.coffee = liquid.Coffee(coffee)
         else: 
-            raise ValueError("must be a coffee")
+            raise TypeError("must be a coffee")
      
      
         if isinstance (price , (int ,float)) and (1.0 <= price <= 10.0):
             self.price = price
         else:
-            raise ValueError("price must be between 1.0 to 10.0") 
+            raise TypeError("price must be between 1.0 to 10.0") 
         
         Order.all_orders.append(self)
         Order.all_coffees.append(self.coffee)
@@ -34,12 +34,12 @@ class Order:
  
          
  
-order1 = Order("yebeee", "mango flavour", 7.0) 
+order1 = Order("7g", "mango flavour", 7.0) 
 order2 = Order("reeeee" , "cappucinoo" , 9.0)
 order3 = Order("maina" , "mango flavour" , 7.0)
 
 
 mango_flavor =("mango flavour")
-print(Order.all_orders)   
+print(order1)   
  
         
